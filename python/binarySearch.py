bd28e12673f3f4a3e0654c3063ffeef6ccaa74f5
goalong <goforalong@gmail.com>
2016-02-28 21:47:35 +0800

    add binarySearch

---
+++ python/binarySearch.py
@@ -0,0 +1,18 @@
+def binarySearch(l, t):
+	low = 0
+	high = len(l) - 1
+	while low < high:
+		mid = (high + low)/2
+		if t > l[mid]:
+			low = mid + 1
+		elif t < l[mid]:
+			high = mid
+		else:
+			return mid
+	return False
+
+l = [1, 4, 12, 45, 66, 99, 120, 444]
+print binarySearch(l, 12)
+print binarySearch(l, 1)
+print binarySearch(l, 13)
+			
